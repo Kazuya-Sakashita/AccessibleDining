9c4157c8ad18d3fb63d0ac77ca80c24a40107b15
Kazuya-Sakashita <kz0508@gmail.com>
2024-03-11 07:09:04 +0900

    住所からの緯度経度登録、詳細ページの地図表示実装

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :restaurant_images, dependent: :destroy
+  geocoded_by :address
+  after_validation :geocode
 
   validates :name, :nearest_station, :budget, :category, :postal_code, :address, :city,
-            :district, :latitude, :longitude, :phone_number, :business_hours, :description, presence: true
+            :district, :phone_number, :business_hours, :description, presence: true
 end
